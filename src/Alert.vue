d97ca2d76cbc31314ec3191f5162cbc408c311fa
guillaume <guillaumechaulet@hotmail.fr>
2020-05-10 20:01:46 +0200

    vue cli part two transition

---
+++ src/Alert.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <transition name="fade">
+        <div class="ui message success" v-if="visible">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui quis nesciunt repellat architecto? Labore praesentium quidem esse incidunt voluptatum! Aut sapiente beatae quisquam itaque natus voluptatum aliquid commodi temporibus alias.
+        </div>
+          </transition>
+        <button @click="toggle">Toggle</button>
+      
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            visible: true
+        }
+    },
+    methods: {
+        toggle: function() {
+            this.visible = !this.visible
+        }
+    }
+}
+</script>
+<style>
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity 1s, transform 1s; 
+    }
+    .fade-enter, .fade-leave-active {
+        opacity: 0;
+        transform: translateX(20px);
+    }
+</style>
+
+
+
+
+
---
+++ src/Counter.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="count">
+        {{ count }}
+        <div class="counter__button" @click="increment">+</div>
+    </div>
+   
+</template>
+
+
+
+<script>
+    import store from './store/NotificationsStore'
+    export default {
+        data () {
+            return {
+                state: store.state
+            }
+        },
+        computed : {
+            count: function() {
+                return this.state.count
+            }
+        },
+        methods: {
+            increment () {
+                store.increment()
+            }
+        }
+    }
+</script>
+
+<style>
+    .counter{
+        font-weight: bold;
+        width: 50px;
+        height: 50px;
+        text-align: center;
+        background-color: #dfdfdf;
+    }
+
+    .counter__button {
+        border: 1px solid #000;
+        padding: 2px 10px;
+    }
+</style>
